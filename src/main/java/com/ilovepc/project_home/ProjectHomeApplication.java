d5817ba24b6084998bcbb313ff1874d2951f58b8
정승주 <ilovesecu@gmail.com>
2025-11-05 15:47:08 +0900

    first commit

---
+++ src/main/java/com/ilovepc/project_home/ProjectHomeApplication.java
@@ -0,0 +1,13 @@
+package com.ilovepc.project_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilovepc/project_home/ProjectHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilovepc.project_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
